79f22ea03725152de7a9c7d6dc9e159518198f03
LamiaaAlaa303 <105085632+LamiaaAlaa303@users.noreply.github.com>
2025-12-07 12:16:38 +0200

    livewire

---
+++ app/Livewire/Admin/Auth/AdminLoginComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin\Auth;
+
+use Livewire\Component;
+
+class AdminLoginComponent extends Component
+{
+    public function render()
+    {
+        return view('admin.auth.admin-login-component');
+    }
+}
